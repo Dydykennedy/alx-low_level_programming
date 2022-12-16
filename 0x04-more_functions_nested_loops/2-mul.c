6a5888398317d56e563e83f1c53a5cf343b92f95
Dydykennedy <didyy21@gmail.com>
2022-12-16 07:00:24 -0800

    my tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *mul - multiplying 2 int
+ * @a: first int
+ * @b: second int
+ * Return: the multiplication result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_line - this function draws a straight line in the terminal
+ *
+ * Return: 
+ */
+
+void print_line(int n)
+{
+
